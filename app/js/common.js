1fa25387f13ea999e7aaea13e4429f761e512588
Andrey <adan3891@gmail.com>
2018-06-03 16:42:56 +0300

    last fix

--- app/js/common.js
+++ app/js/common.js
@@ -5,12 +5,16 @@ $(document).ready(function(){
             $('.headertop').slideUp();
             $('.arrow-top').show(); 
             $('.headermainmenu').addClass('headermainmenu-scroll');
+            $('.logo').addClass('small');
+            $('.container-f').addClass('small');
         }
         
         else {
            $('.headertop').slideDown(); 
            $('.arrow-top').hide(); 
-           $('.headermainmenu').removeClass('headermainmenu-scroll');     
+           $('.headermainmenu').removeClass('headermainmenu-scroll');
+           $('.logo').removeClass('small');  
+           $('.container-f').removeClass('small');   
         }
     });
     
@@ -70,32 +74,22 @@ $('#js-next').click(function () {
     },
     backgroundColor: '#d9f2f8',
     markers: [
-      {latLng: [41.90, 12.45], name: 'Vatican City'},
-      {latLng: [43.73, 7.41], name: 'Monaco'},
-      {latLng: [-0.52, 166.93], name: 'Nauru'},
-      {latLng: [-8.51, 179.21], name: 'Tuvalu'},
-      {latLng: [43.93, 12.46], name: 'San Marino'},
-      {latLng: [47.14, 9.52], name: 'Liechtenstein'},
-      {latLng: [7.11, 171.06], name: 'Marshall Islands'},
-      {latLng: [17.3, -62.73], name: 'Saint Kitts and Nevis'},
-      {latLng: [3.2, 73.22], name: 'Maldives'},
-      {latLng: [35.88, 14.5], name: 'Malta'},
-      {latLng: [12.05, -61.75], name: 'Grenada'},
-      {latLng: [13.16, -61.23], name: 'Saint Vincent and the Grenadines'},
-      {latLng: [13.16, -59.55], name: 'Barbados'},
-      {latLng: [17.11, -61.85], name: 'Antigua and Barbuda'},
-      {latLng: [-4.61, 55.45], name: 'Seychelles'},
-      {latLng: [7.35, 134.46], name: 'Palau'},
-      {latLng: [42.5, 1.51], name: 'Andorra'},
-      {latLng: [14.01, -60.98], name: 'Saint Lucia'},
-      {latLng: [6.91, 158.18], name: 'Federated States of Micronesia'},
-      {latLng: [1.3, 103.8], name: 'Singapore'},
-      {latLng: [1.46, 173.03], name: 'Kiribati'},
-      {latLng: [-21.13, -175.2], name: 'Tonga'},
-      {latLng: [15.3, -61.38], name: 'Dominica'},
-      {latLng: [-20.2, 57.5], name: 'Mauritius'},
-      {latLng: [26.02, 50.55], name: 'Bahrain'},
-      {latLng: [0.33, 6.73], name: 'São Tomé and Príncipe'}
+      {latLng: [52.50, 13.39], name: 'Berlin'},
+        {latLng: [53.56, 10.00], name: 'Hamburg'},
+        {latLng: [48.13, 11.56], name: 'Munich'},
+        {latLng: [50.95, 6.96], name: 'Cologne'},
+        {latLng: [50.11, 8.68], name: 'Frankfurt am Main'},
+        {latLng: [48.77, 9.17], name: 'Stuttgart'},
+        {latLng: [51.23, 6.78], name: 'Düsseldorf'},
+        {latLng: [51.51, 7.46], name: 'Dortmund'},
+        {latLng: [51.45, 7.01], name: 'Essen'},
+        {latLng: [53.07, 8.80], name: 'Bremen'},
+        {latLng: [40.785091, -73.968285], name: 'New York'},
+        {latLng: [36.778259, -119.417931], name: 'California'},
+        {latLng: [47.751076, -120.740135], name: 'Washington'},
+        {latLng: [37.839333, -84.270020], name: 'Kentucky'},
+        {latLng: [39.876019, -117.224121], name: 'Nevada'},
+        
     ]
   })
 
